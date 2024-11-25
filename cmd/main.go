8a3cf85a1c2aa9bbe473a3bf3411c77ff024404d
luisbl03 <Luis.Benito@alu.uclm.es>
2024-11-25 21:05:49 +0100

    signup funcionando correctamente

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/luideiz/API_Go/api"
 	"github.com/luideiz/API_Go/constants"
@@ -13,183 +15,62 @@ func main() {
 	api := gin.Default()
 	api.GET("/version", getVersion) 
 	api.POST("/signup", register)
-	api.GET("/login", login)
-	api.POST("/:username/:doc_id", upload)
+	//api.GET("/login", login)
+	//api.POST("/:username/:doc_id", upload)
 	api.Run(":8080")
 
 }
 
 func getVersion(c *gin.Context) {
 	c.Request.Header.Set("Content-Type", "application/json")
+	log.Println("GET /version")
 	c.JSON(200, gin.H{"version":"1.0.0"})
 }
 
-func register(c* gin.Context) {
-	if !checkBody_User(c) {
-		c.JSON(400, gin.H{"error": "Bad Request"})
+func register(c *gin.Context) {
+	log.Println("POST /signup")
+	valid, json,message := checkBody_user(c)
+	if !valid {
+		c.JSON(400, gin.H{"error":message})
 		return
 	}
-	var request map[string]string
-	c.BindJSON(&request)
-	username := request["username"]
-	password := request["password"]
+	username := json["username"]
+	password := json["password"]
 	status := api.Register(username, password)
 	if status == constants.EXISTS {
-		c.JSON(400, gin.H{"error": "User already exists"})
+		c.JSON(409, gin.H{"error":"user exists"})
 		return
 	}
 	if status == constants.ERROR {
-		c.JSON(500, gin.H{"error": "Internal Server Error"})
+		c.JSON(500, gin.H{"error":"internal error (register)"})
 		return
 	}
-	token := createToken(username)
-	if token.TOKEN == "" {
-		c.JSON(500, gin.H{"error": "Internal Server Error"})
-		return
-	}
-	tokens = append(tokens, token)
-	c.JSON(200, gin.H{"token": token.TOKEN})
-
-}
-
-func login(c* gin.Context) {
-	if !checkBody_User(c) {
-		c.JSON(400, gin.H{"error": "Bad Request"})
-		return
-	}
-	var request map[string]string
-	c.BindJSON(&request)
-	username := request["username"]
-	password := request["password"]
-	status := api.Login(username, password)
-	if status == constants.NOT_FOUND {
-		c.JSON(404, gin.H{"error": "User not found"})
-		return
-	}
-	if status == constants.INVALID_PASSWORD {
-		c.JSON(400, gin.H{"error": "Invalid Password"})
-		return
-	}
-	token := createToken(username)
-	if token.TOKEN == "" {
-		c.JSON(500, gin.H{"error": "Internal Server Error"})
+	token, status := models.CreateToken(username)
+	if status != constants.OK {
+		c.JSON(500, gin.H{"error":"internal error (token)"})
 		return
 	}
 	tokens = append(tokens, token)
-	c.JSON(200, gin.H{"token": token.TOKEN})
+	c.JSON(201, gin.H{"token":token.TOKEN})
 }
 
-func upload(c* gin.Context) {
-	if !checkBody_json(c) {
-		c.JSON(400, gin.H{"error": "Bad Request"})
-		return
-	}
-	if !checkHeader(c) {
-		c.JSON(401, gin.H{"error": "Unauthorized"})
-		return
-	}
-	token_id := c.Request.Header.Get("Authorization")
-	token := getToken(token_id)
-	username := c.Param("username")
-	if !checkToken(token, username) {
-		c.JSON(401, gin.H{"error": "Unauthorized"})
-	}
-	if !checkExpiration(token) {
-		c.JSON(401, gin.H{"error": "Expired Token"})
-	}
-	doc_id := c.Param("doc_id")
-	var data map[string]string
-	c.BindJSON(&data)
-	doc_content := data["doc_content"]
-	status := api.Upload(username, doc_content, doc_id)
-	if status == constants.ERROR {
-		c.JSON(500, gin.H{"error": "Internal Server Error"})
-		return
-	}
-	if status == constants.EXISTS {
-		c.JSON(400, gin.H{"error": "Document already exists"})
-		return
-	}
-	c.JSON(200,gin.H{"size":status})
-}
-
-
-func checkBody_User(c *gin.Context) bool {
-	if c.Request.Body == nil {
-		return false
-	}
-	//miramos si esta el username y el password
+func checkBody_user(c *gin.Context) (bool,map[string]string,string) {
+	//miramos si el cuerpo del mensaje esta vacio
 	var json map[string]string
+	if c.Request.Body == nil {
+		return false,json,"empty body"
+	}
 	err := c.BindJSON(&json)
 	if err != nil {
-		return false
+		return false,json,"invalid json"
 	}
-	if json["username"] == "" || json["password"] == "" {
-		return false
+	//miramos si el campo username esta vacio
+	if json["username"] == "" {
+		return false,json,"empty username"
 	}
-	return true
-
-}
-
-func checkBody_json(c *gin.Context) bool {
-	if c.Request.Body == nil {
-		return false
+	//miramos si el campo password esta vacio
+	if json["password"] == "" {
+		return false,json,"empty password"
 	}
-	var body map[string]string
-	err := c.BindJSON(&body)
-	if err != nil {
-		return false
-	}
-	if body["doc_content"] == "" {
-		return false
-	}
-	return true
-}
-
-func checkHeader(c *gin.Context) bool {
-	if c.Request.Header.Get("Authorization") == "" {
-		return false
-	}
-	return true
-}
-
-func getToken(token string) models.Token {
-	for _, t := range tokens {
-		if t.TOKEN == token {
-			return t
-		}
-	}
-	return models.Token{}
-}
-
-func checkToken(token models.Token, user string) bool {
-	if token.User != user {
-		return false
-	}
-	if !models.IsAlive(token) {
-		return false
-	}
-	return true
-}
-
-func checkExpiration(token models.Token) bool {
-	if !models.IsAlive(token) {
-		//lo borramos de la lista de tokens
-		for i, t := range tokens {
-			if t.TOKEN == token.TOKEN {
-				tokens = append(tokens[:i], tokens[i+1:]...)
-				break
-			}
-		}
-		return false
-	}
-	return true
-}
-
-func createToken(username string) models.Token {
-	token, status := models.CreateToken(username)
-	if status == constants.ERROR {
-		return models.Token{}
-	}
-	return token
+	return true,json,""
 }
