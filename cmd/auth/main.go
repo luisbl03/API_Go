eb70b713f07469c2243e7b6f9b4503a87bcb01de
luisbl03 <Luis.Benito@alu.uclm.es>
2024-12-21 19:59:22 +0100

    resstructuracion de la distribucion de servicios por error en la comprension de la practica y signup funcionando

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,32 +1,29 @@
 package main
 
 import (
-	"bytes"
-	"log"
-	"net/http"
-
-	"encoding/json"
-
 	"github.com/gin-gonic/gin"
-	"github.com/luideoz/API_Go/api"
-	"github.com/luideoz/API_Go/config"
-	"github.com/luideoz/API_Go/constants"
 	"github.com/luideoz/API_Go/models"
+	"github.com/luideoz/API_Go/api"
+	"github.com/luideoz/API_Go/constants"
+	"github.com/luideoz/API_Go/config"
 )
 
+var tokens [] models.Token
 func main() {
-	api := gin.Default()
 	config.Load("config/config.toml")
+	tokens = [] models.Token{}
+	api := gin.Default()
 	api.POST("/signup", signup)
-	api.POST("/login", login)
+
+
 	api.Run(":8081")
 }
 
 func signup(c *gin.Context) {
-	log.Println("POST /signup")
-	valid, user,message := checkBody_user(c)
-	if !valid {
-		c.JSON(400, gin.H{"error":message})
+	var user models.User
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{"error":"invalid json"})
 		return
 	}
 	status := api.Register(user)
@@ -35,89 +32,20 @@ func signup(c *gin.Context) {
 		c.JSON(code, gin.H{"error":msg})
 		return
 	}
-	api.Root(user.USERNAME)
-	body := map[string]string{"username":user.USERNAME}
-	bodyJson, err := json.Marshal(body)
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{"error":"Error creating token (json)"})
+	status = api.Root(user.USERNAME)
+	if status != constants.OK {
+		c.JSON(500, gin.H{"error":"internal error (root)"})
 		return
 	}
-	response, err := http.Post("http://localhost:8082/token", "application/json", bytes.NewBuffer(bodyJson))
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{"error":"Error creating token (post)"})
+	token, status := models.CreateToken(user.USERNAME)
+	if status != constants.OK {
+		c.JSON(500, gin.H{"error":"internal error (token)"})
 		return
 	}
-	var token map[string]string
-	err = json.NewDecoder(response.Body).Decode(&token)
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{"error":"Error creating token (decode)"})
-		return
-	}
-	log.Println("Token: ", token["token"])
-	c.JSON(201, gin.H{"token": token["token"]})
+	tokens = append(tokens, token)
+	c.JSON(201, gin.H{"token":token.TOKEN})
 }
 
-func login (c *gin.Context) {
-	log.Println("GET /login")
-	valid, user, message := checkBody_user(c)
-	if !valid {
-		c.JSON(400, gin.H{"error":message})
-		return
-	}
-	status := api.Login(user)
-	msg, code := Status(status)
-	if msg != "" {
-		c.JSON(code, gin.H{"error":msg})
-		return
-	}
-	body := map[string]string{"username":user.USERNAME}
-	bodyJson, err := json.Marshal(body)
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{"error":"Creating token error (json)"})
-		return
-	}
-	response, err := http.Post("http://localhost:8082/token", "application/json", bytes.NewBuffer(bodyJson))
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{"error":"Creating token error (post)"})
-		return
-	}
-	var token map[string]string
-	err = json.NewDecoder(response.Body).Decode(&token)
-	if err != nil {
-		log.Println(err)
-		c.JSON(500, gin.H{"error":"Creating token error (decode)"})
-		return
-	}
-	log.Println("Token: ", token["token"])
-	c.JSON(200, gin.H{"token": token["token"]})
-
-}
-
-func checkBody_user(c *gin.Context) (bool,models.User,string) {
-	//miramos si el cuerpo del mensaje esta vacio
-	var user models.User
-	if c.Request.Body == nil {
-		return false,user,"empty body"
-	}
-	err := c.BindJSON(&user)
-	if err != nil {
-		return false,user,"invalid json"
-	}
-	//miramos si el campo username esta vacio
-	if user.USERNAME == "" {
-		return false,user,"empty username"
-	}
-	//miramos si el campo password esta vacio
-	if user.PASSWORD == "" {
-		return false,user,"empty password"
-	}
-	return true,user,""
-}
 
 func Status(status int) (string, int) {
 	if status == constants.ERROR {
---
+++ cmd/broker/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/luideoz/API_Go/api"
+	"github.com/luideoz/API_Go/config"
+	"github.com/luideoz/API_Go/models"
+)
+
+func main() {
+    config.Load("config/config.toml")
+    api := gin.Default()
+    api.GET("/version", version)
+    api.POST("/signup", signup)
+
+
+    api.Run(":8080")
+}
+
+func version(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "version": "1.0.0",
+    })
+}
+
+
+func signup(c *gin.Context) {
+    valid, user, message := checkBody_user(c)
+    if !valid {
+        c.JSON(400, gin.H{"error": message})
+        return
+    }
+
+    user.USERNAME = api.Encrypt_hash(user.USERNAME)
+    user.PASSWORD = api.Encrypt_hash(user.PASSWORD)
+
+    jsonData, err := json.Marshal(user)
+    if err != nil {
+        c.JSON(500, gin.H{"error": "internal error, json marshal"})
+        return
+    }
+    response, err := http.Post("http://localhost:8081/signup", "application/json", bytes.NewBuffer(jsonData))
+    if err != nil {
+        c.JSON(500, gin.H{"error": "could not connect to auth service"})
+        return
+    }
+    defer response.Body.Close()
+    var data map[string]string
+    err = json.NewDecoder(response.Body).Decode(&data)
+    if err != nil {
+        c.JSON(500, gin.H{"error": "internal error, json decode"})
+        return
+    }
+    c.JSON(response.StatusCode, data)
+}
+
+func checkBody_user(c *gin.Context) (bool,models.User,string) {
+	//miramos si el cuerpo del mensaje esta vacio
+	var user models.User
+	if c.Request.Body == nil {
+		return false,user,"empty body"
+	}
+	err := c.BindJSON(&user)
+	if err != nil {
+		return false,user,"invalid json"
+	}
+	//miramos si el campo username esta vacio
+	if user.USERNAME == "" {
+		return false,user,"empty username"
+	}
+	//miramos si el campo password esta vacio
+	if user.PASSWORD == "" {
+		return false,user,"empty password"
+	}
+	return true,user,""
+}
